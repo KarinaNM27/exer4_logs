882188bb663d9cd54261316d7948091790afa3af
Karina Nagieva <nagieva1995@list.ru>
2022-05-03 16:40:06 +0500

    fixed #1 resolving

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,10 +2,11 @@ public class Main {
   public static void main(String[] args) {
     BonusService service = new BonusService();
 
-    long amount = 1000_60;
-    boolean registered = true;
 
-    service.calculate(amount, registered);
+    int actual1 = (int) service.calculate(1000_60, true);
+    int expected1 = 30;
+
+    System.out.println("ОР: " + expected1 + ", ФР:" + actual1);
   }
 }
 
